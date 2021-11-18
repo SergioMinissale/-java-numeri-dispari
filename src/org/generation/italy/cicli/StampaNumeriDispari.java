726a9060564f1f52c5003c253ca4c9e0cf7fcdb4
Sergio Minissale <minissale.sergio@outlook.itm>
2021-11-18 16:36:03 +0100

    aggiunto contateggio numeri dispari e pari

--- src/org/generation/italy/cicli/StampaNumeriDispari.java
+++ src/org/generation/italy/cicli/StampaNumeriDispari.java
@@ -15,16 +15,26 @@ public class StampaNumeriDispari {
 		int[] numbers = new int[n];
 		
 		System.out.println("I numeri dispari presenti dentro all'Array sono i seguenti: ");
+		
+		int conteggioNumeriPari = 0;
+		int conteggioNumeriDispari = 0;
 	
 		for(int i = 0; i < numbers.length; i++ ) {
 			numbers[i] = gen.nextInt(100);
-		
+		    
 			if(numbers[i] % 2 != 0) {
-				System.out.println(numbers[i]);		
+				System.out.println(numbers[i]);	
+				conteggioNumeriDispari ++;
+				
+			} else { //conteggio numeri pari
+				conteggioNumeriPari ++;
+				
 			}
-		
+			    
 		}
 		
+		System.out.println("In totale dentro questo Array ci sono: " + conteggioNumeriDispari + " numeri dispari" );
+		System.out.println("In totale dentro a questo Array ci sono: " + conteggioNumeriPari + " numeri pari");	
 				
 		
 		scanner.close();
