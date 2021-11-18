29aa413a4fe35ae2ff77fa4c4a94ba6a18eedace
Sergio Minissale <minissale.sergio@outlook.itm>
2021-11-18 17:17:18 +0100

    bonus finito

--- src/org/generation/italy/cicli/StampaNumeriDispari.java
+++ src/org/generation/italy/cicli/StampaNumeriDispari.java
@@ -12,27 +12,34 @@ public class StampaNumeriDispari {
 		System.out.print("Stabilisci quanti elementi vuoi nell'Array: ");
 		int n= scanner.nextInt();
 		//creo array	
-		int[] numbers = new int[n];
+		int[] numeri = new int[n];
 		
 		System.out.println("I numeri dispari presenti dentro all'Array sono i seguenti: ");
 		
 		int conteggioNumeriPari = 0;
 		int conteggioNumeriDispari = 0;
-	
-		for(int i = 0; i < numbers.length; i++ ) {
-			numbers[i] = gen.nextInt(100);
+	    int somma = 0;
+		for (int i = 0; i < numeri.length; i++ ) {
+			numeri[i] = gen.nextInt(100);
 		    
-			if(numbers[i] % 2 != 0) {
-				System.out.println(numbers[i]);	
+			if (numeri[i] % 2 != 0) {
+				System.out.println(numeri[i]);	
 				conteggioNumeriDispari ++;
 				
 			} else { //conteggio numeri pari
-				conteggioNumeriPari ++;
+				conteggioNumeriPari ++;	
+			}
+			
+			
+			
+			if (numeri[i] > 10 && numeri[i] < 50) {
+				somma += numeri[i];  
+				//somma = somma + numeri[i];
 				
 			}
 			    
 		}
-		
+		System.out.println("La somma dei numeri all'interno dell'Array è: " + somma);
 		System.out.println("In totale dentro questo Array ci sono: " + conteggioNumeriDispari + " numeri dispari" );
 		System.out.println("In totale dentro a questo Array ci sono: " + conteggioNumeriPari + " numeri pari");	
 				
